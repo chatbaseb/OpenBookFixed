11d814ff25d397f87248069bb7c61387aaf038cf
chatbaseb <chatbasebot@gmail.com>
2024-06-15 12:32:50 +0300

    next

--- next.config.js
+++ next.config.js
@@ -1,33 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   future: {
-//     webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
-//     // Looks like backward compatibility approach.
-//   },
-//   webpack(config) {
-//     config.resolve.fallback = {
-//       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-//       // by next.js will be dropped. Doesn't make much sense, but how it is
-//       fs: false, // the solution
-//     };
-
-//     return config;
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-// },
-// typescript: {
-//   // !! WARN !!
-//   // Dangerously allow production builds to successfully complete even if
-//   // your project has type errors.
-//   // !! WARN !!
-//   ignoreBuildErrors: true,
-// },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -44,6 +14,16 @@ const nextConfig = {
 
     return config;
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+},
+typescript: {
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true,
+},
 };
 
 module.exports = nextConfig;
